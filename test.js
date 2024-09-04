4617e5e29e9485e94e870bd4ad339d8a0eadd107
achuu177 <aswathikp120@gmail.com>
2024-09-04 17:41:43 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+function checkOddOrEven(number) {
+
+    if (number % 2 === 0) {
+        console.log('Even');
+    } else {
+        console.log('Odd');
+    }
+}
+
+
+checkOddOrEven(5);  
+checkOddOrEven(10); 
